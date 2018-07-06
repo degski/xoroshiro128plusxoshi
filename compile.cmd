e6a40ce98a1949986927ea457f49d2309da70abe
degski <degski@gmail.com>
2018-07-06 12:16:26 +0300

    updates

---
+++ compile.cmd
@@ -0,0 +1,3 @@
+@ clang-cl -fuse-ld=lld -flto=thin  /D "NDEBUG" /D "_CONSOLE" /D "NOMINMAX" /D "_UNICODE" /D "UNICODE" -Xclang -fcxx-exceptions /Ox /Oi /MT main.cpp -Xclang -std=c++2a -Xclang -ffast-math -mmmx  -msse  -msse2 -msse3 -mssse3 -msse4.1 -msse4.2 -mavx -mavx2
+
+
