80c863a73863a99eeaec211f1b3c7d4d7ef53804
degski <degski@gmail.com>
2018-07-24 18:24:52 +0300

    copyright description

--- uniform_int_distribution_fast.hpp
+++ uniform_int_distribution_fast.hpp
@@ -4,7 +4,7 @@
 // C++17-compliant uniform_int_distribution_fast, based on bounded_rand-function
 // as per the paper by Daniel Lemire:
 // https://arxiv.org/abs/1805.10941.
-// Optimizations added to bounded_rand by Melissa E. O'Neill:
+// and optimizations added to bounded_rand by Melissa E. O'Neill:
 // http://www.pcg-random.org/posts/bounded-rands.html
 //
 // Copyright (c) 2018 degski
